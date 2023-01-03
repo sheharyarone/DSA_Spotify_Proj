0f9da1b55e33dd60ce26ad524fe4b5be44f937cd
Sheharyar <sheharyarone@gmail.com>
2023-01-03 09:58:53 +0500

    final

--- main.cpp
+++ main.cpp
@@ -8,15 +8,12 @@ int main()
     ArtistHashTable *ArtistContainer = new ArtistHashTable(53131);
     parseArtistCSV("DataSet/ArtistNames.csv", ArtistContainer);
 
-    cout << "ARTIST CONTAINER SET" << endl;
     AVLTree<Genre> *GenreContainer = new AVLTree<Genre>;
     parseGenreCSV("DataSet/Genre.csv", GenreContainer);
-    cout << "GENRE CONTAINER SET" << endl;
     Genre *pointer = GenreContainer->Search("afrobeat");
 
     TrackHashTable *TrackContainer = new TrackHashTable(82109);
     parseCSV("DataSet/dataset.csv", ArtistContainer, TrackContainer, GenreContainer);
-    cout << "TRACKS HASHTABLE SET" << endl;
 
     // cout << "NO OF Unique Artists WERE : " << noOfArtist << endl;
     // cout << "No of Collisions in Artist were : " << noOfCollisionsInArtist << endl;
